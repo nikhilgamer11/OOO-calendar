bf5cdeec69d2365964f91b67b198ebde8e3f14b7
nikhilgamer11 <84565989+nikhilgamer11@users.noreply.github.com>
2025-10-23 10:25:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // This tells Vercel to look in the root folder for index.html, 
-    // and correctly handle the base path for assets.
-    base: './', 
-  }
-})
+});
