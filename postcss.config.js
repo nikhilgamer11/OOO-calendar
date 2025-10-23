7a71e74599b3431979913a1409e09ff3eb8d3303
nikhilgamer11 <84565989+nikhilgamer11@users.noreply.github.com>
2025-10-23 10:26:09 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,6 @@
-[Immersive content redacted for brevity.]
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
