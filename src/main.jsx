58ab9ff766d2fdbf9e43f38b66a61c4eb338b1f9
nikhilgamer11 <84565989+nikhilgamer11@users.noreply.github.com>
2025-10-23 10:27:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-    import React from 'react'
-    import ReactDOM from 'react-dom/client'
-    import App from './App.jsx'
-    import './index.css' 
-    
-    // This loads the entire application from App.jsx
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-    )
-    
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // <-- required for Tailwind
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
