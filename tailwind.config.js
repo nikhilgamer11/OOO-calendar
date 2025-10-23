d7a13b99fa6ea0dfcbd7d823a0da336a850c0337
nikhilgamer11 <84565989+nikhilgamer11@users.noreply.github.com>
2025-10-23 10:39:13 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      boxShadow: {
-        soft: "0 10px 25px -10px rgba(0,0,0,0.15)"
-      }
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
